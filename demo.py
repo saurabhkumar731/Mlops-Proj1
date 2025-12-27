d846f39eb1fe7434a45c69c586748dc2c0f655a8
Saurabh <kumarsaurabh00435@gamil.com>
2025-12-27 17:47:10 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -20,4 +20,9 @@
 #     logging.info(e)
 #     raise MyException(e, sys) from e
 
- 
+# --------------------------------------------------------------------------------
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline() 
